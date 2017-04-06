99fadf0617ad2cda4fb59ca5269dfe52f644c9c8
nik frank <nikf@sweetinn.com>
2017-04-06 23:45:35 +0300

    add testing dep, psuedocode fro the simplest test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Game from './Game';
+
+import { mount } from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Game />, div);
+});
+
+it('computes a calculation', ()=>{
+  const p = mount(<Game/>);
+
+  // now make a game
+
+  // pull the code
+
+  // imvent a shitty guess
+
+  // then test a bunch of times that the guess is wrong how youd' expect it to be
+
+  // then expect the results to be as you'd expect (once you've won the game the right number of times)
+
 });
